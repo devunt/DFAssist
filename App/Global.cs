7588063c92f0ab09fcc72ce786f5297b3a4f56ad
Junehyeon Bae <devunt@gmail.com>
2016-10-11 18:24:09 +0900

    v20161011.1 릴리즈

--- App/Global.cs
+++ App/Global.cs
@@ -2,7 +2,7 @@
 {
     class Global
     {
-        public const string VERSION = "v20161001.2";
+        public const string VERSION = "v20161011.1";
 
         public const string APPNAME = "DFAssist";
         public const string SETTINGS_FILEPATH = @"config.ini";
