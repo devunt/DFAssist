48a9c750a4c1c051f4d8f3ee55d8c4e49678f7e7
Dominic Bindley <dbindley@gmail.com>
2017-09-05 23:32:18 -0700

    Added mutex.

--- App/Program.cs
+++ App/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace App
 {
     internal static class Program
     {
+        private const string AppName = "DFAssist";
+
+        private static Mutex _mutex;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main()
+        {
+            try
+            {
+                Mutex.OpenExisting(AppName);
+            }
+            catch
+            {
+                _mutex = new Mutex(true, AppName);
+                AppStart();
+            }
+        }
+
+        private static void AppStart()
         {
 #if !DEBUG
             Sentry.Initialise();
